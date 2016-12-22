98e24ef01c6e9feec95732c0102f0b7ee299f692
Brian Dutz <bgdutz@gmail.com>
2016-12-22 10:58:26 -0800

    switch to canvas instead of OpenGL, add basic ship functionality/movement

--- app/src/main/java/com/epicodus/gravityapp/MainActivity.java
+++ app/src/main/java/com/epicodus/gravityapp/MainActivity.java
@@ -5,68 +5,40 @@ import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private GLSurfaceView mGLView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mGLView = new openGLSurfaceView(this);
-        setContentView(mGLView);
+        final OpenGLRenderer draw = new OpenGLRenderer(this);
+        setContentView(draw);
+        draw.requestFocus();
+        draw.setOnTouchListener(new View.OnTouchListener()
+
+        {
+
+            @Override
+            public boolean onTouch (View v, MotionEvent e)
+            {
+                draw.x = e.getX();
+                draw.y = e.getY();
+                draw.invalidate();
+                return true;
+            }
+
+
+        });
     }
 
-public class openGLSurfaceView extends GLSurfaceView {
-
-        private final OpenGLRenderer mRenderer;
-        private final float TOUCH_SCALE_FACTOR = 180.0f/320;
-        private float mPreviousX;
-        private float mPreviousY;
-
-    @Override
-    public boolean onTouchEvent(MotionEvent e)
-    {
-        float x = e.getX();
-        float y = e.getY();
-
-        switch(e.getAction()){
-            case MotionEvent.ACTION_MOVE:
-                float dx = x - mPreviousX;
-                float dy = y - mPreviousY;
-
-                if (y>getHeight()/2){
-                    dx = dx * -1;
-                }
-                if (x>getWidth()/2){
-                    dy = dy * -1;
-                }
-
-                mRenderer.setAngle(
-                        mRenderer.getAngle() + ((dx-dy) * TOUCH_SCALE_FACTOR));
-                requestRender();
-
-        }
-        mPreviousX = x;
-        mPreviousY = y;
-        return true;
-    }
-
-        public openGLSurfaceView(Context context){
-            super(context);
 
 
-            setEGLContextClientVersion(2);
-
-            mRenderer = new OpenGLRenderer();
+}
 
 
-            setRenderer(mRenderer);
-            setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
-        }
-    }
 
-    }
 
 
--- app/src/main/java/com/epicodus/gravityapp/OpenGLRenderer.java
+++ app/src/main/java/com/epicodus/gravityapp/OpenGLRenderer.java
@@ -1,62 +1,59 @@
 package com.epicodus.gravityapp;
 
+import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.opengl.Matrix;
 import android.os.SystemClock;
+import android.view.View;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
-public class OpenGLRenderer implements GLSurfaceView.Renderer {
+public class OpenGLRenderer extends View {
 
-    private Triangle mTriangle;
-    private final float[] mMVPMatrix = new float[16];
-    private final float[] mProjectionMatrix = new float[16];
-    private final float[] mViewMatrix = new float[16];
-    private float[] mRotMatrix = new float[16];
-    public volatile float mAngle;
 
-    public float getAngle() {
-        return mAngle;
-    }
+    Paint paint;
+    Context context;
+    float x;
+    float y;
 
-    public void setAngle(float angle) {
-        mAngle=angle;
+    public OpenGLRenderer(Context context) {
+        super(context);
+        paint = new Paint();
+        paint.setColor(Color.RED);
+        x = 100.0f;
+        y = 100.0f;
     }
 
 
 
     @Override
-    public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
-        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        mTriangle = new Triangle();
+    public void onDraw(Canvas canvas) {
+        paint.setColor(Color.BLACK);
+        canvas.drawPaint(paint);
+        paint.setStrokeWidth(2);
+        paint.setColor(Color.RED);
+
+        Point vert1 = new Point((int)x,(int)y );
+        Point vert2 = new Point((int)x,(int)y+100);
+        Point vert3 = new Point((int)x+75,(int)y+50);
+
+        Path p = new Path();
+        p.setFillType(Path.FillType.EVEN_ODD);
+        p.moveTo(vert1.x, vert1.y);
+        p.lineTo(vert2.x, vert2.y);
+        p.lineTo(vert3.x, vert3.y);
+        p.lineTo(vert1.x, vert1.y);
+        p.close();
+
+        canvas.drawPath(p, paint);
     }
 
-    @Override
-    public void onDrawFrame(GL10 gl10) {
-        float[] scratch = new float[16];
-//        long time = SystemClock.uptimeMillis() % 4000L;
-//        float angle = 0.090f * ((int)time);
-        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-        Matrix.setRotateM(mRotMatrix,0,mAngle,0,0,-1.0f);
-        Matrix.setLookAtM(mViewMatrix,0,0,0,5f,0f,0f,0f,0f,1.0f,0.0f);
-        Matrix.multiplyMM(mMVPMatrix,0,mProjectionMatrix,0,mViewMatrix, 0);
-        Matrix.multiplyMM(scratch,0, mMVPMatrix,0, mRotMatrix, 0);
-        Matrix.translateM(mTriangle.mMatrix, 0, 0.5f,0f,0f);
-        mTriangle.draw(scratch);
-    }
-    @Override
-    public void onSurfaceChanged(GL10 gl10, int width, int height) {
-        GLES20.glViewport(0, 0, width, height);
-        float ratio = (float) width/height;
-        Matrix.frustumM(mProjectionMatrix, 0, -ratio, ratio,-1,1,1,10);
-    }
-    public static int myShader(int type, String code) {
-        int shader = GLES20.glCreateShader(type);
-        GLES20.glShaderSource(shader, code);
-        GLES20.glCompileShader(shader);
-        return shader;
-
-    }
 }
